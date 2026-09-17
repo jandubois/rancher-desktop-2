6acb13e64eea7f3436aa5be83944c2a851711d26
Mark Yen <mark.yen@suse.com>
2026-09-17 14:04:34 -0700

    RDD: API: make mirror names bijective
    If the original name (e.g. for a containerd namespace) matches the format
    we use for encoded names, we also encode the string.  Also update the
    documentation to match.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- rdd/pkg/util/api/mirror_name.go
+++ rdd/pkg/util/api/mirror_name.go
@@ -7,17 +7,30 @@ package api
 
 import (
 	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"strings"
 
 	"k8s.io/apimachinery/pkg/util/validation"
 )
 
 // MirrorName returns the mirrored name for a given container resource.  The
 // prefix is prepended to the mirrored name with a dash if the input is not a
-// valid Kubernetes object name.
+// valid Kubernetes object name, or if it looks like the encoded output of this
+// function.
 func MirrorName(prefix, input string) string {
-	if len(validation.IsDNS1123Subdomain(input)) < 1 {
+	hasher := sha256.New()
+	needsEscape := len(validation.IsDNS1123Subdomain(input)) > 0
+	if !needsEscape {
+		after, found := strings.CutPrefix(input, prefix+"-")
+		if found && len(after) == hex.EncodedLen(hasher.Size()) {
+			// Check that the string only consists of hexadecimal characters.
+			needsEscape = strings.TrimLeft(after, "0123456789abcdef") == ""
+		}
+	}
+	if !needsEscape {
 		return input
 	}
-	return fmt.Sprintf("%s-%x", prefix, sha256.Sum256([]byte(input)))
+	_, _ = hasher.Write([]byte(input))
+	return fmt.Sprintf("%s-%x", prefix, hasher.Sum(nil))
 }
