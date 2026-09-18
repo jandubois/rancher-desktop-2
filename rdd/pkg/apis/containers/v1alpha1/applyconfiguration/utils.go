mook-as/rd2

RDD: Containers: Create Compose API objects
This defines the API object, without any implementation.

Signed-off-by: Mark Yen <mark.yen@suse.com>

rdd/pkg/apis/containers/v1alpha1/applyconfiguration/utils.go
@@ -16,6 +16,18 @@ import (
 func ForKind(kind schema.GroupVersionKind) interface{} {
 	switch kind {
 	// Group=containers.rancherdesktop.io, Version=v1alpha1
+	case v1alpha1.SchemeGroupVersion.WithKind("Compose"):
+		return &containersv1alpha1.ComposeApplyConfiguration{}
+	case v1alpha1.SchemeGroupVersion.WithKind("ComposeMember"):
+		return &containersv1alpha1.ComposeMemberApplyConfiguration{}
+	case v1alpha1.SchemeGroupVersion.WithKind("ComposeStatus"):
+		return &containersv1alpha1.ComposeStatusApplyConfiguration{}
+	case v1alpha1.SchemeGroupVersion.WithKind("ComposeUpRequest"):
+		return &containersv1alpha1.ComposeUpRequestApplyConfiguration{}
+	case v1alpha1.SchemeGroupVersion.WithKind("ComposeUpRequestSpec"):
+		return &containersv1alpha1.ComposeUpRequestSpecApplyConfiguration{}
+	case v1alpha1.SchemeGroupVersion.WithKind("ComposeUpRequestStatus"):
+		return &containersv1alpha1.ComposeUpRequestStatusApplyConfiguration{}
 	case v1alpha1.SchemeGroupVersion.WithKind("Container"):
 		return &containersv1alpha1.ContainerApplyConfiguration{}
 	case v1alpha1.SchemeGroupVersion.WithKind("ContainerLastAction"):

rdd/pkg/apis/containers/v1alpha1/zz_generated.deepcopy.go
@@ -10,6 +10,212 @@ import (
 )
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *Compose) DeepCopyInto(out *Compose) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Status.DeepCopyInto(&out.Status)
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new Compose.
+func (in *Compose) DeepCopy() *Compose {
+	if in == nil {
+		return nil
+	}
+	out := new(Compose)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *Compose) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ComposeList) DeepCopyInto(out *ComposeList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]Compose, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ComposeList.
+func (in *ComposeList) DeepCopy() *ComposeList {
+	if in == nil {
+		return nil
+	}
+	out := new(ComposeList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *ComposeList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ComposeMember) DeepCopyInto(out *ComposeMember) {
+	*out = *in
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ComposeMember.
+func (in *ComposeMember) DeepCopy() *ComposeMember {
+	if in == nil {
+		return nil
+	}
+	out := new(ComposeMember)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ComposeStatus) DeepCopyInto(out *ComposeStatus) {
+	*out = *in
+	if in.Configs != nil {
+		in, out := &in.Configs, &out.Configs
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	if in.Members != nil {
+		in, out := &in.Members, &out.Members
+		*out = make([]ComposeMember, len(*in))
+		copy(*out, *in)
+	}
+	if in.Conditions != nil {
+		in, out := &in.Conditions, &out.Conditions
+		*out = make([]v1.Condition, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ComposeStatus.
+func (in *ComposeStatus) DeepCopy() *ComposeStatus {
+	if in == nil {
+		return nil
+	}
+	out := new(ComposeStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ComposeUpRequest) DeepCopyInto(out *ComposeUpRequest) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	in.Status.DeepCopyInto(&out.Status)
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ComposeUpRequest.
+func (in *ComposeUpRequest) DeepCopy() *ComposeUpRequest {
+	if in == nil {
+		return nil
+	}
+	out := new(ComposeUpRequest)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *ComposeUpRequest) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ComposeUpRequestList) DeepCopyInto(out *ComposeUpRequestList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]ComposeUpRequest, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ComposeUpRequestList.
+func (in *ComposeUpRequestList) DeepCopy() *ComposeUpRequestList {
+	if in == nil {
+		return nil
+	}
+	out := new(ComposeUpRequestList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *ComposeUpRequestList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ComposeUpRequestSpec) DeepCopyInto(out *ComposeUpRequestSpec) {
+	*out = *in
+	if in.Configs != nil {
+		in, out := &in.Configs, &out.Configs
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ComposeUpRequestSpec.
+func (in *ComposeUpRequestSpec) DeepCopy() *ComposeUpRequestSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(ComposeUpRequestSpec)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ComposeUpRequestStatus) DeepCopyInto(out *ComposeUpRequestStatus) {
+	*out = *in
+	if in.Conditions != nil {
+		in, out := &in.Conditions, &out.Conditions
+		*out = make([]v1.Condition, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ComposeUpRequestStatus.
+func (in *ComposeUpRequestStatus) DeepCopy() *ComposeUpRequestStatus {
+	if in == nil {
+		return nil
+	}
+	out := new(ComposeUpRequestStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *Container) DeepCopyInto(out *Container) {
 	*out = *in
 	out.TypeMeta = in.TypeMeta

jandubois/rancher-desktop-2

rdd/pkg/apis/containers/v1alpha1/applyconfiguration/utils.go
@@ -16,6 +16,18 @@ import (
 func ForKind(kind schema.GroupVersionKind) interface{} {
 	switch kind {
 	// Group=containers.rancherdesktop.io, Version=v1alpha1
+	case v1alpha1.SchemeGroupVersion.WithKind("ComposeProject"):
+		return &containersv1alpha1.ComposeProjectApplyConfiguration{}
+	case v1alpha1.SchemeGroupVersion.WithKind("ComposeProjectContainer"):
+		return &containersv1alpha1.ComposeProjectContainerApplyConfiguration{}
+	case v1alpha1.SchemeGroupVersion.WithKind("ComposeProjectStatus"):
+		return &containersv1alpha1.ComposeProjectStatusApplyConfiguration{}
+	case v1alpha1.SchemeGroupVersion.WithKind("ComposeUpRequest"):
+		return &containersv1alpha1.ComposeUpRequestApplyConfiguration{}
+	case v1alpha1.SchemeGroupVersion.WithKind("ComposeUpRequestSpec"):
+		return &containersv1alpha1.ComposeUpRequestSpecApplyConfiguration{}
+	case v1alpha1.SchemeGroupVersion.WithKind("ComposeUpRequestStatus"):
+		return &containersv1alpha1.ComposeUpRequestStatusApplyConfiguration{}
 	case v1alpha1.SchemeGroupVersion.WithKind("Container"):
 		return &containersv1alpha1.ContainerApplyConfiguration{}
 	case v1alpha1.SchemeGroupVersion.WithKind("ContainerLastAction"):

rdd/pkg/apis/containers/v1alpha1/zz_generated.deepcopy.go
@@ -10,6 +10,212 @@ import (
 )
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ComposeProject) DeepCopyInto(out *ComposeProject) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Status.DeepCopyInto(&out.Status)
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ComposeProject.
+func (in *ComposeProject) DeepCopy() *ComposeProject {
+	if in == nil {
+		return nil
+	}
+	out := new(ComposeProject)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *ComposeProject) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ComposeProjectContainer) DeepCopyInto(out *ComposeProjectContainer) {
+	*out = *in
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ComposeProjectContainer.
+func (in *ComposeProjectContainer) DeepCopy() *ComposeProjectContainer {
+	if in == nil {
+		return nil
+	}
+	out := new(ComposeProjectContainer)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ComposeProjectList) DeepCopyInto(out *ComposeProjectList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]ComposeProject, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ComposeProjectList.
+func (in *ComposeProjectList) DeepCopy() *ComposeProjectList {
+	if in == nil {
+		return nil
+	}
+	out := new(ComposeProjectList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *ComposeProjectList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ComposeProjectStatus) DeepCopyInto(out *ComposeProjectStatus) {
+	*out = *in
+	if in.Configs != nil {
+		in, out := &in.Configs, &out.Configs
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	if in.Containers != nil {
+		in, out := &in.Containers, &out.Containers
+		*out = make([]ComposeProjectContainer, len(*in))
+		copy(*out, *in)
+	}
+	if in.Conditions != nil {
+		in, out := &in.Conditions, &out.Conditions
+		*out = make([]v1.Condition, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ComposeProjectStatus.
+func (in *ComposeProjectStatus) DeepCopy() *ComposeProjectStatus {
+	if in == nil {
+		return nil
+	}
+	out := new(ComposeProjectStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ComposeUpRequest) DeepCopyInto(out *ComposeUpRequest) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	in.Status.DeepCopyInto(&out.Status)
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ComposeUpRequest.
+func (in *ComposeUpRequest) DeepCopy() *ComposeUpRequest {
+	if in == nil {
+		return nil
+	}
+	out := new(ComposeUpRequest)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *ComposeUpRequest) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ComposeUpRequestList) DeepCopyInto(out *ComposeUpRequestList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]ComposeUpRequest, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ComposeUpRequestList.
+func (in *ComposeUpRequestList) DeepCopy() *ComposeUpRequestList {
+	if in == nil {
+		return nil
+	}
+	out := new(ComposeUpRequestList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *ComposeUpRequestList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ComposeUpRequestSpec) DeepCopyInto(out *ComposeUpRequestSpec) {
+	*out = *in
+	if in.Configs != nil {
+		in, out := &in.Configs, &out.Configs
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ComposeUpRequestSpec.
+func (in *ComposeUpRequestSpec) DeepCopy() *ComposeUpRequestSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(ComposeUpRequestSpec)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ComposeUpRequestStatus) DeepCopyInto(out *ComposeUpRequestStatus) {
+	*out = *in
+	if in.Conditions != nil {
+		in, out := &in.Conditions, &out.Conditions
+		*out = make([]v1.Condition, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ComposeUpRequestStatus.
+func (in *ComposeUpRequestStatus) DeepCopy() *ComposeUpRequestStatus {
+	if in == nil {
+		return nil
+	}
+	out := new(ComposeUpRequestStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *Container) DeepCopyInto(out *Container) {
 	*out = *in
 	out.TypeMeta = in.TypeMeta